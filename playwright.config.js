6f1042b17a80be7080715a11fad3e711b2e169fd
Beatriz Kailane <beatrizkailany3@gmail.com>
2026-09-03 14:53:21 -0300

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,12 +1,17 @@
-﻿import { defineConfig } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   timeout: 200000, // 2min por teste
-
   use: {
     baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
     video: 'retain-on-failure',
     screenshot: 'only-on-failure',
+  },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120000, // 2min pra subir o servidor
   },
 });
